8b39d71f8549eec3954d1c715c754c8cf6cd7315
Meghashyam Yellapu <143782652+shyamyellapu@users.noreply.github.com>
2024-12-20 19:45:22 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (22.91%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int xor1=0;
-       for(int i=0;i<nums.size();i++){
-           xor1 ^=nums[i];
-       }
-        return xor1;
+        // Sort the array
+        sort(nums.begin(), nums.end());
+
+        // Traverse the array in pairs
+        for (int i = 0; i < nums.size() - 1; i += 2) {
+            // If a pair is not equal, the single number is found
+            if (nums[i] != nums[i + 1]) {
+                return nums[i];
+            }
+        }
+
+        // If no unmatched pair is found, the single number is the last element
+        return nums.back();
     }
 };
