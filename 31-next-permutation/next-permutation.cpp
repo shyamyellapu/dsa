2e6c277e5a6829c3b8794490b1e9e6af17475885
Meghashyam Yellapu <143782652+shyamyellapu@users.noreply.github.com>
2024-12-23 22:11:07 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (14.26%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,46 +1,7 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) 
-    {
-        // 2 pointer technique
-        // left, right, breakpoint
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(),nums.end());
         
-        //break point = point where increasing seq from end comes to halt
-        //left = element before break point
-        // right = element just greater than left
-        // swap laft and right
-        // sort elements from next to left till end
-        
-
-        int n = nums.size();
-
-        int left, breakpoint, right;
-        right = n-1 ;
-        while( (right-1>=0) && (nums[right]<=nums[right-1]) )
-            right--;
-
-        if (right == 0)
-        {
-            sort(nums.begin(), nums.end());
-        }
-        else
-        {
-            left = right-1;
-            breakpoint = right;
-            int mini = INT_MAX;
-            for(int i = breakpoint; i < n ; i++)
-            {
-                if (nums[i] < mini && nums[i] >nums[left])
-                {
-                    right = i;
-                    mini = nums[i];
-                }
-            }
-
-            swap(nums[left], nums[right]);
-            sort(nums.begin()+breakpoint, nums.end());
-        }
-
-    return ;
     }
 };
