1e365b0a1e68b00158745d45c46685bd44978aa2
Meghashyam Yellapu <143782652+shyamyellapu@users.noreply.github.com>
2025-01-02 22:21:28 +0530

    Time: 4 ms (69.50%) | Memory: 23.8 MB (58.22%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -5,22 +5,12 @@ public:
         vector<vector<int>> ans;
         sort(arr.begin(),arr.end());
         for(int i=0;i<n;i++){
-            int start =arr[i][0];
-            int end = arr[i][1];
-            if(!ans.empty()&& end<=ans.back()[1]){
-                continue;
-
+            if(ans.empty()|| arr[i][0]>ans.back()[1]){
+                ans.push_back(arr[i]);
             }
-            
-            for(int j=i+1;j<n;j++){
-                if(arr[j][0]<=end){
-                    end=max(end,arr[j][1]);
-                }
-                else{
-                    break;
-                }
+            else{
+                ans.back()[1]=max(ans.back()[1],arr[i][1]);
             }
-            ans.push_back({start,end});
         }
       return ans;  
     }
